225a8d5a2da8f35296adce1b3327ad0a39877fcd
simonmader17 <mail@simonmader.at>
2023-07-14 15:45:44 +0200

    Added default data

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,7 +2,18 @@
 export async function load({ params }) {
 	let fuelprices;
 
-	await fetch('http://api.fuelprices.box').then((res) => (fuelprices = res.json()));
+	await fetch('http://api.fuelprices.box')
+		.then((res) => (fuelprices = res.json()))
+		.catch(
+			(error) =>
+				(fuelprices = [
+					{ timestamp: '2023-07-14T09:00:00Z', avanti: 1.52, jet: 1.51 },
+					{ timestamp: '2023-07-14T10:00:00Z', avanti: 1.55, jet: 1.54 },
+					{ timestamp: '2023-07-14T11:00:00Z', avanti: 1.53, jet: 1.54 },
+					{ timestamp: '2023-07-14T12:00:00Z', avanti: 1.535, jet: 1.52 },
+					{ timestamp: '2023-07-14T13:00:00Z', avanti: 1.545, jet: 1.53 }
+				])
+		);
 
 	return { fuelprices };
 }
